23b853e846b62ff58b6942d51550551680aae5ff
Valerii Diachenko <leriy.dyachenko@gmail.com>
2017-06-21 23:30:08 +0300

    Сервис

---
+++ compile.bat
@@ -0,0 +1,3 @@
+@rem call opm update vanessa-runner
+
+call vanessa-runner update-dev --src ./src/cf
---
+++ decompile.bat
@@ -0,0 +1,4 @@
+@rem call opm update vanessa-runner
+
+
+call runner-old decompilecurrent ./src/cf --ibname /F".\build\ib" 
